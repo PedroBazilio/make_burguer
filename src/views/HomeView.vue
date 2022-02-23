7c352333717aabd64f50d8ddd37b3a90bd839f6e
PedroBazilio <pedrobazilio@id.uff.br>
2022-02-23 10:47:23 -0300

    banner feito

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <template>
+  <banner/>
   <div class="main-container">
-   <h1>INÍCIO</h1>
+   <h1>Monte seu Pedido:</h1>
+   <p>formulario</p>
   </div>
 </template>
+
+<script>
+import Banner from '../components/Banner.vue'
+
+export default {
+  name: 'Home',
+  components: {Banner}  
+  
+}
+</script>
 
 
 <style >
