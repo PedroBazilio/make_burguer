142d5a53ae1ca74daaf969f0e929d1b6c0560deb
PedroBazilio <pedrobazilio@id.uff.br>
2022-02-24 11:31:34 -0300

    pegando dados do back e enviando para o dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+      <div id="burger-table">
+          <div>
+              <div id="burger-table-heading">
+                  <div class="order-id">#</div>
+                  <div>Cliente</div>
+                  <div>Pão</div>
+                  <div>Carne</div>
+                  <div>Acompanhamentos</div>
+                  <div>Ações:</div>
+              </div>
+          </div>
+          <div class="burger-table-rows">
+              <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                  <div class="order-number">{{ burger.id }}</div>
+                  <div>{{ burger.nome }}</div>
+                  <div>{{ burger.pao }}</div>
+                  <div>{{ burger.carne }}</div>
+                  <div>
+                      <ul v-for="(opcional, index) in burger.opcionais" :key="index">
+                          <li>{{ opcional }}</li>
+                      </ul>
+                  </div>
+                  <div>
+                      <select name="status" classe="status">
+                          <option value="">Status</option>
+                      </select>
+                      <button class="delete-btn">Cancelar</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Dashboard',
+    data() {
+      return {
+        burgers: null,
+        burgerId: null,
+        status: []
+      }
+    },
+    methods: {
+      async getPedidos() {
+        const req = await fetch('http://localhost:3000/burgers')
+        const data = await req.json()
+        this.burgers = data
+        // Resgata os status de pedidos
+        //this.getStatus()
+      },
+    },
+    mounted() {
+      this.getPedidos();
+    },
+}
+</script>
+
+<style scoped>
+    #burger-table {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  #burger-table-heading,
+  #burger-table-rows,
+  .burger-table-row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #burger-table-heading {
+    font-weight: bold;
+    padding: 12px;
+    border-bottom: 3px solid #333;
+  }
+  .burger-table-row {
+    width: 100%;
+    padding: 12px;
+    border-bottom: 1px solid #CCC;
+  }
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
+  }
+  #burger-table-heading .order-id,
+  .burger-table-row .order-number {
+    width: 5%;
+  }
+  select {
+    padding: 12px 6px;
+    margin-right: 12px;
+  }
+  .delete-btn {
+    background-color: rgb(73, 58, 58);
+    color: rgb(241, 231, 236);
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: 0.5s;
+  }
+  
+  .delete-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+</style>
--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,191 +1,203 @@
 <template>
-<div>
+  <div>
     <Message :msg="msg" v-show="msg" />
-</div>
-<div id="burguer-form-div">
-    <form id="burguer-form" @submit="createBurger">
-        <div class="input-container">
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="name"
-             v-model="nome" placeholder="Digite o seu nome">
+  </div>
+  <div id="burger-form-div">
+    <form id="burger-form" @submit="createBurger">
+      <div class="input-container">
+        <label for="nome">Nome:</label>
+        <input
+          type="text"
+          id="nome"
+          name="name"
+          v-model="nome"
+          placeholder="Digite o seu nome"
+        />
+      </div>
+      <div class="input-container">
+        <label for="pao">Escolha o tipo de pão</label>
+        <select name="pao" id="pao" v-model="pao">
+          <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+            {{ pao.tipo }}
+          </option>
+        </select>
+      </div>
+      <div class="input-container">
+        <label for="carne">Escolha o tipo de carne</label>
+        <select name="carne" id="carne" v-model="carne">
+          <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+            {{ carne.tipo }}
+          </option>
+        </select>
+      </div>
+      <div id="opcionais-container" class="input-container">
+        <label id="opcionais-title" for="opcionais"
+          >Escolha os acompanhamentos</label
+        >
+        <div
+          class="checkbox-container"
+          v-for="opcional in opcionaisData"
+          :key="opcional.id"
+        >
+          <input
+            type="checkbox"
+            name="opcionais"
+            v-model="opcionais"
+            :value="opcional.tipo"
+          />
+          <span>{{ opcional.tipo }}</span>
         </div>
-         <div class="input-container">
-            <label for="pao">Escolha o tipo de pão</label>
-            <select name="pao" id="pao" v-model="pao">
-                <option v-for="pao in paes" :key='pao.id' :value="pao.tipo">
-                    {{pao.tipo}}
-                </option>
-            </select>
-        </div>
-        <div class="input-container">
-            <label for="carne">Escolha o tipo de carne</label>
-            <select name="carne" id="carne" v-model="carne">
-                <option v-for="carne in  carnes" :key="carne.id" :value="carne.tipo">{{carne.tipo}}</option>
-            </select>
-        </div>
-        <div id="opcionais-container" class="input-container">
-            <label id="opcionais-title" for="opcionais">Escolha os acompanhamentos</label>
-            <div class="checkbox-container" v-for="opcional in opcionaisData" :key="opcional.id">
-                <input type="checkbox" name="opcionais" 
-                v-model="opcionais" :value="opcional.tipo">
-                <span>{{opcional.tipo}}</span>
-            </div>
-        </div>
-        <div class="input-container">
-            <input type="submit" class="submit-btn" value="Criar pedido">
-        </div>
-
-         
+      </div>
+      <div class="input-container">
+        <input type="submit" class="submit-btn" value="Criar pedido" />
+      </div>
     </form>
-</div>
-  
+  </div>
 </template>
 
 <script>
-import Message from './Message.vue'
+import Message from "./Message.vue";
 
 export default {
-    name:'Form',
-    components: {Message},
-    data() {
-        return {
-            paes: null,
-            carnes: null,
-            opcionaisData: null,
-            nome: null,
-            carne:null,
-            pao: null,
-            opcionais: [],
-            msg: null,
-        }
-    },
-    methods:{
-        async getIngredientes(){
-            const req = await fetch('http://localhost:3000/ingredientes');
-            const data = await req.json();
+  name: "Form",
+  components: { Message },
+  data() {
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisData: null,
+      nome: null,
+      carne: null,
+      pao: null,
+      opcionais: [],
+      msg: null,
+    };
+  },
+  methods: {
+    async getIngredientes() {
+      const req = await fetch("http://localhost:3000/ingredientes");
+      const data = await req.json();
 
-            this.paes = data.paes;
-            this.carnes = data.carnes;
-            this.opcionaisData = data.opcionais
-        },
-        async createBurger(e){
-
-            e.preventDefault();
-            
-            const data = {
-                nome: this.nome,
-                carne: this.carne,
-                pao: this.pao,
-                opcionais: Array.from(this.opcionais),
-                status: 'Solicitado'
-            }
-            console.log(this.carne)
-            const dataJson = JSON.stringify(data);
-            const req = await fetch('http://localhost:3000/burgers',{
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: dataJson
-            });
-            const res = await req.json();
-            
-            //msg do sistema
-            this.msg = `Pedido Nº${res.id} realizado com sucesso!`;
-            //limpar msg
-            setTimeout(() => {
-               this.msg = ''
-            }, 5000);
-            // limpeza dos campos
-            this.nome = '';
-            this.carne = '';
-            this.pao = '';
-            this.opcionais = '';
-        }
+      this.paes = data.paes;
+      this.carnes = data.carnes;
+      this.opcionaisData = data.opcionais;
     },
-    mounted() {
-        this.getIngredientes()
-    },
+    async createBurger(e) {
+      e.preventDefault();
 
-}
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+      console.log(this.carne);
+      const dataJson = JSON.stringify(data);
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+      const res = await req.json();
+
+      //msg do sistema
+      this.msg = `Pedido Nº${res.id} realizado com sucesso!`;
+      //limpar msg
+      setTimeout(() => {
+        this.msg = "";
+      }, 5000);
+      // limpeza dos campos
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = "";
+    },
+  },
+  mounted() {
+    this.getIngredientes();
+  },
+};
 </script>
 
 <style scoped>
+#burger-form-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-    #burguer-form-div{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    
-    #burguer-form{
-        max-width: 400px;
-        margin: 0 auto;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-    .input-container{
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-    }
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
 
-    label{
-        font-weight: bold;
-        margin-bottom: 15px;
-        color: rgb(77, 73, 73);
-        padding: 5px 10px;
-        border-left: 4px solid rgb(143, 109, 126);;
-    }
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: rgb(77, 73, 73);
+  padding: 5px 10px;
+  border-left: 4px solid rgb(143, 109, 126);
+}
 
-    input, select {
-        padding: 5px 10px;
-        width: 300px;
-    }
+input,
+select {
+  padding: 5px 10px;
+  width: 300px;
+}
 
-    #opcionais-container {
-        flex-direction: row;
-        flex-wrap: wrap;
-        margin-left: 50px;
-    }
+#opcionais-container {
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-left: 50px;
+}
 
-    #opcionais-title{
-       width: 100%; 
-    }
+#opcionais-title {
+  width: 100%;
+}
 
-    .checkbox-container{
-        display: flex;
-        align-items: flex-start;
-        width: 50%;
-        margin-bottom: 20px;
-    }
+.checkbox-container {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
+}
 
-    .checkbox-container span,
-    .checkbox-container input{
-        width: auto;
-    }
-    .checkbox-container input{
-        margin-top: 3px;
-    }
-    .checkbox-container span{
-        margin-left: 6px;
-        font-weight: bold;
-    }
+.checkbox-container span,
+.checkbox-container input {
+  width: auto;
+}
+.checkbox-container input {
+  margin-top: 3px;
+}
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
+}
 
-    .submit-btn{
-        background-color: rgb(73, 58, 58);
-        color: rgb(241, 231, 236);
-        font-weight: bold;
-        border: 2px solid #222;
-        padding: 10px;
-        font-size: 16px;
-        margin: 0 auto;
-        cursor: pointer;
-        transition: .5s;
-    }
+.submit-btn {
+  background-color: rgb(73, 58, 58);
+  color: rgb(241, 231, 236);
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
 
-    .submit-btn:hover{
-        background-color: transparent;
-        color: #222;
-    }
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
 </style>
